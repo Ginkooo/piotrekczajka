bd03d56493557819c6159ac4dda964863245444e
Piotr Czajka <czajka@protonmail.com>
2018-07-24 07:27:07 +0200

    Getting post for detail view leaved like it is, maybe I will fix it someday

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.utils import timezone
 
 from blog.forms import EmailPostForm
 from blog.models import Post
@@ -38,7 +41,9 @@ class PostListView(ListView):
 
 
 def post_detail(request, year, month, day, slug):
-    post = get_object_or_404(Post, slug=slug, status='published',
-                             published__year=year, published__month=month,
-                             published__day=day)
+    post = Post.active.get(slug=slug,
+                           published__year=year)
+    # TODO:Why the hell is it not working?
+
+
     return render(request, 'blog/post/detail.html', {'post': post})
